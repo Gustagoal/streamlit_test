5aeca55b30315da8719ff19ca9e8e98d13cc38a9
Gustagoal <gustavocastruu@gmail.com>
2025-07-21 22:31:38 -0300

    subindo os arquivos do streamlit

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st  
+import pandas as pd
+
+dados = pd.read_excel('bd_vendas.xlsx')
+
+
+st.title('Ola, tudo bem?')
+
+nome = st.text_input('Digite seu nome : ')
+
+if nome:
+    st.text(f' Seja bem vindo {nome}')
+
+
+st.area_chart(dados,x='Produto',y='Qtde') #Produto Qtde 
+
+
+
