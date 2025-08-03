bd955ad0c099498f5743c002fdbcd74a4390852f
Gustagoal <gustavocastruu@gmail.com>
2025-08-03 14:03:26 -0300

    Subindo o arquivo em excel

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import streamlit as st  
 import pandas as pd
 
+dados = pd.read_excel('bd_vendas.xlsx')
+print(dados.head())
+
 
 st.title('Ola, tudo bem?')
 
@@ -8,6 +11,11 @@ nome = st.text_input('Digite seu nome : ')
 
 if nome:
     st.text(f' Seja bem vindo {nome}')
+
+
+st.data_editor(dados)
+
+
 
 
 
