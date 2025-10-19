4a4bdd47cce61047ec803e2f71baad8d1a2804ea
unknown <gustavocastruu@gmail.com>
2025-10-19 12:59:42 -0300

    Gerando graficos com streamlit

--- dashbord/graficos_st.py
+++ dashbord/graficos_st.py
@@ -1,11 +1,14 @@
 import streamlit as st 
 import pandas as pd 
 
+with st.sidebar:
+    upload = st.file_uploader("Insira seu arquivo")
+    if upload:
+     st.success("Arquivo inserido com sucesso")
 
-df = pd.read_excel(r"C:\Users\Gustavo Castro\Desktop\streamlit_test\dashbord\planilha_unica.xlsx")
+    df = pd.read_excel(upload)
+    st.radio(label="Selecione as opções abaixo",options=df['Categoria'].unique())
 
 
-st.title("Olá seja bem vindo")
-st.data_editor(df)
-st.bar_chart(df["Categoria"])
-
+st.subheader("Planilha de custos")
+st.bar_chart(df,x= "Nome_Produto", y= "Preco_Unit")
